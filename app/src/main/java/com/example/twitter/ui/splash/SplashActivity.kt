fc5e80bc96518f449aece70cad0cb2f8a77ed407
Andrey Malakhov <andrii.malakhov@chisw.com>
2019-03-21 12:09:25 +0200

    add splash screen

---
+++ app/src/main/java/com/example/twitter/ui/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package com.example.twitter.ui.splash
+
+import android.app.Activity
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+	}
+}
