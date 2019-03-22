d9ad2dffd56dd6558a70fc4b965c812420770314
Andrey Malakhov <andrii.malakhov@chisw.com>
2019-03-22 13:39:08 +0200

    set username to the sent tweet

--- app/src/main/java/com/example/twitter/domain/usecases/SendTweetUseCase.kt
+++ app/src/main/java/com/example/twitter/domain/usecases/SendTweetUseCase.kt
@@ -3,25 +3,28 @@ package com.example.twitter.domain.usecases
 import com.example.twitter.data.api.ApiResponse
 import com.example.twitter.data.api.RestApi
 import com.example.twitter.data.dto.Tweet
+import com.example.twitter.data.repositories.UserRepository
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class SendTweetUseCase @Inject constructor(
-	private val api: RestApi
+	private val api: RestApi,
+	private val userRepository: UserRepository
 ) {
 	fun send(message: String): Single<out ApiResponse<Tweet>> {
-		// todo get username
-		val username = ""
-		// todo ideally, time should be obtained from an injected repository for better testability
-		val timeMs = System.currentTimeMillis()
+		return userRepository.getUsername()
+			.flatMap { username ->
+				// todo ideally, time should be obtained from an injected repository for better testability
+				val timeMs = System.currentTimeMillis()
 
-		val tweet = Tweet(username, timeMs, message)
-		return api.postTweet(tweet)
-			// todo inject
-			.subscribeOn(Schedulers.io())
-			// todo inject
-			.observeOn(AndroidSchedulers.mainThread())
+				val tweet = Tweet(username, timeMs, message)
+				api.postTweet(tweet)
+					// todo inject
+					.subscribeOn(Schedulers.io())
+					// todo inject
+					.observeOn(AndroidSchedulers.mainThread())
+			}
 	}
 }
