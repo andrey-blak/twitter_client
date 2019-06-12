9e9628243929327433186f696fa57ac1108ca85c
Andrey Malakhov <andrii.malakhov@chisw.com>
2019-06-12 15:33:21 +0300

    fix exception in android tests

--- app/src/androidTest/java/com/example/twitter/daggerMockRule.kt
+++ app/src/androidTest/java/com/example/twitter/daggerMockRule.kt
@@ -6,10 +6,15 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.example.twitter.ui.TwitterApp
 import com.example.twitter.ui.di.AppComponent
 import com.example.twitter.ui.di.modules.ApiModule
+import com.example.twitter.ui.di.modules.DatabaseModule
 import com.example.twitter.ui.di.modules.PreferencesModule
 import it.cosenonjaviste.daggermock.DaggerMock
 
-fun daggerMockRule() = DaggerMock.rule<AppComponent>(ApiModule(), PreferencesModule(app)) {
+fun daggerMockRule() = DaggerMock.rule<AppComponent>(
+	ApiModule(),
+	PreferencesModule(app),
+	DatabaseModule(app)
+) {
 	set { component -> TwitterApp.component = component }
 }
 
