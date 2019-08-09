b9c018b9b5bf81e09f294f9f932637d71601dd77
Andrey Malakhov <andrii.malakhov@chisw.com>
2019-08-09 15:55:00 +0300

    add invalid login test

--- app/src/androidTest/java/com/example/twitter/ui/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/twitter/ui/login/LoginActivityTest.kt
@@ -1,9 +1,17 @@
 package com.example.twitter.ui.login
 
+import android.app.Activity
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import com.example.twitter.R
 import com.example.twitter.daggerMockRule
@@ -14,6 +22,8 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Single
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -37,10 +47,37 @@ class LoginActivityTest {
 
 		activityRule.launchActivity(null)
 
-		Espresso.onView(ViewMatchers.withId(R.id.login_login_button))
-			.perform(ViewActions.click())
+		Espresso.closeSoftKeyboard()
 
-		Espresso.onView(ViewMatchers.withId(R.id.tweets_recyclerview))
-			.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+		onView(withId(R.id.login_login_button))
+			.perform(click())
+
+		onView(withId(R.id.tweets_recyclerview))
+			.check(matches(isDisplayed()))
+	}
+
+	@Test
+	fun testInvalidLogin() {
+		whenever(api.login(any(), any()))
+			.thenReturn(Single.just(ApiResponse.Error()))
+
+		activityRule.launchActivity(null)
+
+		onView(withId(R.id.login_username))
+			.perform(replaceText("asd@asd.com"), closeSoftKeyboard())
+
+		onView(withId(R.id.login_password))
+			.perform(replaceText("invalid_password"), closeSoftKeyboard())
+
+		onView(withId(R.id.login_login_button))
+			.perform(click())
+
+		onToast(activityRule.activity, R.string.login_login_error)
+			.check(matches(isDisplayed()))
+	}
+
+	private fun onToast(activity: Activity, messageResId: Int): ViewInteraction {
+		return onView(withText(messageResId))
+			.inRoot(withDecorView(not(`is`(activity.window.decorView))))
 	}
 }
